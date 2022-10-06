3420ce2bc946fe41a3d12c8bff18199408f47b48
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2022-10-06 17:22:08 +0300

    1st_revision

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const imagesContainer = document.querySelector(`.gallery`);
+const imagesMarkup = createImgCardMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imagesContainer.addEventListener('click', onImageContainerClick);
+
+function createImgCardMarkup(galleryItems) {     
+return galleryItems.map(({preview, original, description}) => {
+  return `<div class = "gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;}).join('');
+}
+///-----Modal window -----
+
+function onImageContainerClick(evt) {
+  evt.preventDefault();
+if (!evt.target.nodeName === 'img'){
+    return;
+  }
+
+const onActiveImage = evt.target.getAttribute('data-source')
+
+const instance = basicLightbox.create(`
+<img src ="${onActiveImage}">)`)
+instance.show()
+
+imagesContainer.addEventListener('keydown', evt => {
+  if (evt.code === 'Escape') {
+    instance.close()
+  }
+})
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector(`.gallery`);
+const imagesMarkup = createImgCardMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+function createImgCardMarkup(galleryItems) {     
+    return galleryItems.map(({preview, original, description}) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+      ;}).join('');
+      }
+
+const lightbox = new SimpleLightbox(`.gallery a`, { captionsData: "alt", captionPosition:'bottom', captionDelay: 250 });
